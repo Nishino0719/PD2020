56e3415e55cc0176981e4b6c26f4473a904ff33c
Nishino0719 <nishino0719@icloud.com>
2020-06-16 11:29:43 +0900

    とりあえず終わらなかったので提出課題用をつくりました号泣

---
+++ 0616/0616-192C1113-1/html-report.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+#include "kion-htmlreport.h"
+#include "kion-htmlreport2.h"
+#include "kion-htmlreport3.h"
+#include "kionstat.h"
+
+#define DAYARRAY 24
+
+/* HTMLによる気温表の出力 */
+void html_report(struct kisyou array[], int size)
+{
+    double kion[DAYARRAY];
+    int i, di;
+
+    /* HTMLのヘッダー部分 */
+    printf("<html>\n<head>\n");
+    printf("<title>気温データ</title>\n");
+    printf("</head>\n");
+    /* 文書の本体の開始 */
+    printf("<bodyY>\n");
+    /* 見出しの表示 */
+    printf("<h1>日別気温統計データ</h1>\n");
+    /* 表のヘッダー部分の開始 */
+    printf("<table border=1>\n");
+    printf("<tr><th>月日</th><th>平均</th><th>最高</th><th>最低</th><th>日較差</th></tr>\n");
+
+    /* 表の本体部分の出力 */
+    for(i = 0; i < size; i += DAYARRAY){ /* 一日単位で処理 */
+	for(di = 0; di < DAYARRAY; di++){
+	    /* １日分の気温を統計計算用の配列に転記 */
+	    kion[di] = array[i + di].kion;
+	}
+
+	/* 表の本体の１行出力 */
+	printf("<tr><td>%d月%d日</td><td>%.1f</td><td>%.1f</td><td>%.1f</td>",
+			array[i].month, array[i].day, 
+			kion_heikin(kion, DAYARRAY),
+			kion_max(kion, DAYARRAY),
+			kion_min(kion, DAYARRAY));
+    red_nitikakusa(kion_nitikakusa(kion_max(kion, DAYARRAY),kion_min(kion, DAYARRAY)));
+
+    }
+
+    
+
+    printf("</table>\n"); /* 表の終端 */
+    printf("</body>\n"); /* 文書本体の終端 */
+    printf("</html>\n"); /* HTMLの終端 */
+}
---
+++ 0616/0616-192C1113-1/kion-htmlreport2.h
@@ -0,0 +1,2 @@
+// 日較差を返す
+double kion_nitikakusa(double max,double min);
