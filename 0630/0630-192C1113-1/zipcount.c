889fcfa960af77152a7c45b66e4976783cf802de
Nishino0719 <nishino0719@icloud.com>
2020-06-29 23:28:12 +0900

    提出用ファイルを編集しました。

--- 0630/0630-192C1113-1/zipcount.c
+++ 0630/0630-192C1113-1/zipcount.c
@@ -7,16 +7,7 @@
 #define	MAXLINE	200
 #define	MAXPREFNAME	20
 #define MAXPREFNUM	100
-
-// struct pref_zip_count{
-//     char pref_name[MAXPREFNAME];
-//     int zipcount;
-// };
-
 #define	NOT_FOUND	-1
-// int zip_pref_search(struct pref_zip_count data[], int size, char prefname[]);
-// void zip_pref_add(struct pref_zip_count data[], int pos, char prefname[]);
-// void zip_pref_print(struct pref_zip_count data[], int size);
 
 int main(void)
 {   
@@ -53,34 +44,3 @@ int main(void)
 
     return 0;
 }
-
-/* 都道府県名が配列に既に登録されていればその添え字を返す */
-// int zip_pref_search(struct pref_zip_count data[], int size, char prefname[])
-// {
-//     int i;
-
-//     for(i = 0; i < size; i++){ /* 都道府県名の検索 */
-// 	if(strcmp(prefname, data[i].pref_name) == 0){
-// 	    return i;
-// 	}
-//     }
-
-//     return NOT_FOUND; /* 見つからない */
-// }
-
-/* 都道府県名を配列に登録する */
-// void zip_pref_add(struct pref_zip_count data[], int pos, char prefname[])
-// {
-//     strcpy(data[pos].pref_name, prefname); /* 都道府県名の登録 */
-//     data[pos].zipcount = 1; /* 郵便番号数の初期化 */
-// }
-
-/* 配列に記録されている郵便番号数と都道府県名の組を出力する */
-// void zip_pref_print(struct pref_zip_count data[], int size)
-// {
-//     int i, prefcount;
-
-//     for(i = 0; i < size; i++){ /* 郵便番号数と都道府県名を表示 */
-// 	printf("%d: %s\n", data[i].zipcount, data[i].pref_name);
-//     }
-// }
